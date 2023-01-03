caf0957a8eaefffa23915db2dba62218444ce126
Noel Glezam <100516405+Drakamell@users.noreply.github.com>
2023-01-03 23:51:43 +0100

    Add files via upload

--- python/reto_00.py
+++ python/reto_00.py
@@ -9,9 +9,9 @@ cada impresión), sustituyendo los siguientes:
 
 for x in range (1,101):
     if x % 3 == 0:
-        print ("buzz")
-    elif x % 5 == 0:
         print ("fizz")
+    elif x % 5 == 0:
+        print ("buzz")
     elif x % 3 == 0 and x % 5 == 0:
         print("fizzbuzz")
     else:
